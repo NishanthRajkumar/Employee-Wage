19084db3680cf517b2e9da13ad2dcb26bb94b2d5
Nishanth <rajkumarnishanth@gmail.com>
2022-02-08 17:37:18 +0530

    Added the daily wage calculation method

--- EmployeeWageApp/Program.cs
+++ EmployeeWageApp/Program.cs
@@ -4,4 +4,5 @@ Console.Title = "Employee Wage App";
 Console.WriteLine("-----------------Employee Wage Calculation App-----------------");
 EmployeeWage employee = new EmployeeWage();
 employee.GetAttendance();
+employee.GetDailyWage();
 Console.ReadKey();
