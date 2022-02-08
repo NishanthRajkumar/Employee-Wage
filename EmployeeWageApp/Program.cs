0ff10331602fa255d80b6b9ef88f7390b43ddd40
Nishanth <rajkumarnishanth@gmail.com>
2022-02-08 17:54:45 +0530

    Added a method to calculate monthly wage

--- EmployeeWageApp/Program.cs
+++ EmployeeWageApp/Program.cs
@@ -3,6 +3,5 @@
 Console.Title = "Employee Wage App";
 Console.WriteLine("-----------------Employee Wage Calculation App-----------------");
 EmployeeWage employee = new EmployeeWage();
-employee.GetAttendance();
-employee.GetDailyWage();
+employee.MonthlyWage();
 Console.ReadKey();
