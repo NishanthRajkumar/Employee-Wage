4d676ef329a333e53cbed7b61601c9a76a4fec86
Nishanth <rajkumarnishanth@gmail.com>
2022-02-14 12:31:42 +0530

    added company list; enhanced encapsulation & readabilty

---
+++ EmployeeWageApp/CompanyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageApp;
+
+
+internal class CompanyList
+{
+    // Declaring class attribute of type Dictionary
+    private Dictionary<string, EmployeeWage> companies;
+
+    // Initialise the dictionary
+    public CompanyList()
+    {
+        companies = new();
+    }
+
+    // Add company name and uses default parameters for EmployeeWage and meets wage condition
+    public void AddCompany(string companyName)
+    {
+        companies.Add(companyName, new EmployeeWage());
+        companies[companyName].MeetWageCondition();
+    }
+
+    // Add company with additional parameters  and meets wage condition
+    public void AddCompany(string companyName, int ratePerHour, int maxWorkingDays, int maxHoursPerMonth)
+    {
+        companies.Add(companyName, new EmployeeWage(ratePerHour, maxWorkingDays, maxHoursPerMonth));
+        companies[companyName].MeetWageCondition();
+    }
+
+    // Display all companies
+    public void Display()
+    {
+        foreach (var company in companies)
+            Console.WriteLine("\nCompany: " + company.Key + "\n\tWage details:\n\t" + company.Value.ToString());
+    }
+}
--- EmployeeWageApp/Program.cs
+++ EmployeeWageApp/Program.cs
@@ -2,7 +2,18 @@
 
 Console.Title = "Employee Wage App";
 Console.WriteLine("-----------------Employee Wage Calculation App-----------------");
-EmployeeWage employee = new EmployeeWage();
-employee.MeetWageCondition();
-employee.Display();
+
+// Creating an object of CompanyList
+CompanyList companyList = new();
+
+// Adding company with default values for wage, number of working days and working hours per month
+companyList.AddCompany("Reliance");
+companyList.AddCompany("Tata");
+
+// Adding company with parameters for wage, number of working days and working hours per month
+companyList.AddCompany("Facebook", 50, 22, 120);
+
+// Display all companies
+companyList.Display();
+
 Console.ReadKey();
